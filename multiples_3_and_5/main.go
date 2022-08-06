f5528cd09c56385605c6272adabc19332d104c65
Carht <carht@protonmail.com>
2022-08-06 00:36:22 -0400

    Test for multiplesOfThree.

--- multiples_3_and_5/main.go
+++ multiples_3_and_5/main.go
@@ -1,1 +1,13 @@
 package main
+
+func multiplesOfThree(s []int) []int {
+	mulThree := []int{}
+
+	for _, i := range s {
+		if i % 3 == 0 {
+			mulThree = append(mulThree, i)
+		}
+	}
+
+	return mulThree
+}
--- multiples_3_and_5/main_test.go
+++ multiples_3_and_5/main_test.go
@@ -3,9 +3,35 @@ package main
 import "testing"
 
 func TestMultiplesOfThree(t *testing.T) {
-	result := multiplesOfThree([]int{1,2,3,4,5,6,7,8,9})
+	result := multiplesOfThree([]int{3, 6, 9})
+	expected := []int{3, 6, 9}
 
-	if result != []int{3,6,9} {
-		t.Error("Expected slice is %v", result)
+	for i := range result {
+		if result[i] != expected[i] {
+			t.Errorf("Expected slice is %v", result)
+		}
 	}
 }
+
+func testMultiplesOfThree(t *testing.T) {
+	result := multiplesOfThree([]int{1, 3, 6, 9})
+	expected := []int{3, 6, 9}
+
+	for i := range result {
+		if result[i] != expected[i] {
+			t.Errorf("Expected slice is %v", result)
+		}
+	}
+}
+
+func testMultipleOfThree(t *testing.T) {
+	result := multiplesOfThree([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20})
+	expected := []int{3, 6, 9, 12, 15, 18}
+
+	for i := range result {
+		if result[i] != expected[i] {
+			t.Errorf("Expected slice is %v", result)
+		}
+	}
+}
+
